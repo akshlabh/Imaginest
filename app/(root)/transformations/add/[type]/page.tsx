5845e0e0c37c7e3166cc7361f3a39a6eeae145a6
akshay <akshaylabh08@gmail.com>
2025-04-17 22:44:09 +0530

    test10

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,12 +1,15 @@
 import Header from '@/components/shared/header'
-// import TransformationForm from '@/components/shared/TransformationForm';
+import TransformationForm from '@/components/shared/TransformationForm'
 import { transformationTypes } from '@/constants'
 
+interface SearchParamProps {
+  params: {
+    type: keyof typeof transformationTypes
+  }
+}
 
-const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-
-  const transformation = transformationTypes[type];
-
+const AddTransformationTypePage = ({ params }: SearchParamProps) => {
+  const transformation = transformationTypes[params.type]
 
   return (
     <>
@@ -14,7 +17,7 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-    
+      <TransformationForm />
     </>
   )
 }
